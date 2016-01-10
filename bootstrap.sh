878fa0a8af9e6960362c8c0a910fbf4a2e350c8e
Adam J. Stewart <ajstewart426@gmail.com>
2016-01-10 11:17:57 -0600

    Added Sublime Text settings file synchronization

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,8 +10,27 @@ git pull origin master
 
 function syncDotfiles {
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-          --exclude "README.md" -avh --no-perms . ~
+          --exclude "*.sublime-settings" --exclude "README.md" -avh --no-perms . ~
     source ~/.bashrc
+
+    # Sync Sublime Text settings files
+    kernel=$(uname)
+    case "$kernel" in
+        'Darwin') # OS X
+            sublimeDir="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+            ;;
+        'Linux')
+            sublimeDir="$HOME/.config/sublime-text-3/Packages/User"
+            ;;
+        *)
+            echo "Unknown OS: $kernel"
+            ;;
+    esac
+
+    if [[ -d "$sublimeDir" ]]
+    then
+        rsync --include "*.sublime-settings" -avh --no-perms . "$sublimeDir"
+    fi
 }
 
 if [[ "$1" == "--force" || "$1" == "-f" ]]
