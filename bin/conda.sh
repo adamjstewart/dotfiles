fa947b893617fac3b575d5573f4a867636d5024f
Adam J. Stewart <ajstewart426@gmail.com>
2019-10-19 12:48:46 -0500

    No longer using Homebrew or Anaconda

--- bin/conda.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Install Python libraries using conda
-
-# Update all installed packages
-conda update --all --yes
-
-# Remove unused packages and caches
-conda clean --all --yes
